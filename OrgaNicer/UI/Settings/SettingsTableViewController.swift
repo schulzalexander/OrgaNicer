33137fe961a718a55fb02d223f618486b33f0da7
Alexander Schulz <alexanderschulz94@gmx.de>
2018-09-15 12:17:58 +0200

    New Settings page, reset section

--- OrgaNicer/UI/Settings/SettingsTableViewController.swift
+++ OrgaNicer/UI/Settings/SettingsTableViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class SettingsTableViewController: UITableViewController {
 
 	//MARK: Outlets
-	@IBOutlet weak var resetButton: UIButton!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +19,46 @@ class SettingsTableViewController: UITableViewController {
 			self.navigationItem.largeTitleDisplayMode = .never
 		}
 		
-		setupResetButton()
     }
 	
-	@IBAction func resetContent(_ sender: UIButton) {
+	//MARK: UITableViewDelegate
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if indexPath.section == 0 {
+			// Reset
+			if indexPath.row == 0 {
+				// Alarm Reset
+				resetAlarms()
+			}
+			if indexPath.row == 1 {
+				// Complete Content Reset
+				resetContent()
+			}
+		}
+	}
+	
+	//MARK: Content Reset
+	
+	private func resetAlarms() {
+		let alertController = UIAlertController(title: NSLocalizedString("Attention", comment: ""), message: NSLocalizedString("AlarmResetAlertMessage", comment: ""), preferredStyle: .alert)
+		let reset = UIAlertAction(title: NSLocalizedString("Delete", comment: ""), style: .destructive) { (action) in
+			AlarmManager.removeAllAlarms()
+			TaskManager.shared.tasks.forEach({ (key, val) in
+				if let mainTask = val as? MainTask {
+					mainTask.alarm = nil
+					TaskArchive.saveTask(task: mainTask)
+				}
+			})
+			self.tableView.selectRow(at: nil, animated: true, scrollPosition: .none)
+		}
+		let cancel = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel) { (action) in
+			self.tableView.selectRow(at: nil, animated: true, scrollPosition: .none)
+		}
+		alertController.addAction(reset)
+		alertController.addAction(cancel)
+		present(alertController, animated: true, completion: nil)
+	}
+	
+	private func resetContent() {
 		let alertController = UIAlertController(title: NSLocalizedString("Attention", comment: ""), message: NSLocalizedString("ContentResetAlertMessage", comment: ""), preferredStyle: .alert)
 		let reset = UIAlertAction(title: NSLocalizedString("Reset", comment: ""), style: .destructive) { (action) in
 			guard let taskTable = self.navigationController?.viewControllers.first as? TaskTableViewController else {
@@ -39,16 +74,12 @@ class SettingsTableViewController: UITableViewController {
 			taskTable.navigationItem.title = nil
 			self.navigationController?.popToRootViewController(animated: true)
 		}
-		let cancel = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil)
+		let cancel = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel) { (action) in
+			self.tableView.selectRow(at: nil, animated: true, scrollPosition: .none)
+		}
 		alertController.addAction(reset)
 		alertController.addAction(cancel)
 		present(alertController, animated: true, completion: nil)
-	}
-	
-	private func setupResetButton() {
-		resetButton.backgroundColor = UIColor.red
-		resetButton.layer.cornerRadius = resetButton.frame.height / 2
-		resetButton.clipsToBounds = true
 	}
 	
 }
