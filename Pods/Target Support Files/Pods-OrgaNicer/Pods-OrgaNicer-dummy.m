46c30974280cc26bd55af396047c7e081b31309b
Alexander Schulz <alexanderschulz94@gmx.de>
2018-09-24 09:13:29 +0200

    remove Pods directory from repo

--- Pods/Target Support Files/Pods-OrgaNicer/Pods-OrgaNicer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrgaNicer : NSObject
-@end
-@implementation PodsDummy_Pods_OrgaNicer
-@end
--- Pods/Target Support Files/Pods-OrgaNicerUITests/Pods-OrgaNicerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrgaNicerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OrgaNicerUITests
-@end
